de09e6817efadc5d03ace1fd21fcd627ebe5b339
Sureshkrishh <ksureshkrishna97@gmail.com>
2021-03-18 10:21:32 +0530

    python revision notes

---
+++ trail.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def ud_sum():
+    x = int(input('Enter first number: '))
+    y = int(input('Enter second number: '))
+    z = x+y
+    print(z)
+
+if __name__ == '__main__':
+    ud_sum()
+
