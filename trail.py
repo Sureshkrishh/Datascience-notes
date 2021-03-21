c30bfe57eec96993d4e74d39bb0b57cb7302c155
Sureshkrishh <ksureshkrishna97@gmail.com>
2021-03-21 13:00:46 +0530

    Python revision notes

--- trail.py
+++ trail.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[1]:
 
 
 def ud_sum():
@@ -10,6 +10,24 @@ def ud_sum():
     z = x+y
     print(z)
 
+def ud_sub():
+    x = int(input('Enter first number: '))
+    y = int(input('Enter second number: '))
+    if x>y:
+        z = x-y
+        print(z)
+    else:
+        z = y-x
+        print(z)
+    
 if __name__ == '__main__':
     ud_sum()
+if __name__ == '__main__':
+    ud_sub()
+
+
+# In[ ]:
+
+
+
 
